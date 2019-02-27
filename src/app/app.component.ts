4d13ac554e69765edc0bc7a9b959f18e99cd4da2
Thanasis Dimitriadis <thanasis.dimitriadis@mitsishotels.com>
2019-02-27 18:40:12 +0200

    Add modal trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularmodalService} from '../../projects/angularmodal/src/lib/angularmodal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cook-angular-modal';
+
+  constructor(public modalService: AngularmodalService){}
 }
